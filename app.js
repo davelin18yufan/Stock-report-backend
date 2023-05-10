7530fa64d16909f39ed66805f359d3eae27be060
davelin18yufan <davelin30630@gmail.com>
2023-05-10 17:15:58 +0800

    fix:modify config.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
+require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const routes = require('./routes')
