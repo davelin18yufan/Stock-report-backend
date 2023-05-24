2009e35e689cae148a91984a67bde887006ea031
davelin18yufan <davelin30630@gmail.com>
2023-05-24 14:04:36 +0800

    fix: adjust body parser limitation

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const SESSION_SECRET = process.env.SESSION_SECRET
 
 app.use(cors())
 // body-parser
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 app.use(session({ secret: SESSION_SECRET || 'secret', resave: false, saveUninitialized: false }))
 // passport
